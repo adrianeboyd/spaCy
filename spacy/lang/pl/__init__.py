8593fce90eff4ac646ba3868740d619a1201ce7a
Adriane Boyd <adrianeboyd@gmail.com>
2020-03-25 22:01:34 +0100

    TEMP: Polish tokenization

--- spacy/lang/pl/__init__.py
+++ spacy/lang/pl/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
-from .punctuation import TOKENIZER_INFIXES
+from .punctuation import TOKENIZER_INFIXES, TOKENIZER_SUFFIXES
 from .tag_map import TAG_MAP
 from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
@@ -21,10 +21,14 @@ class PolishDefaults(Language.Defaults):
     lex_attr_getters[NORM] = add_lookups(
         Language.Defaults.lex_attr_getters[NORM], BASE_NORMS
     )
-    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
+    mod_base_exceptions = {
+        exc: val for exc, val in BASE_EXCEPTIONS.items() if not exc.endswith(".")
+    }
+    tokenizer_exceptions = update_exc(mod_base_exceptions, TOKENIZER_EXCEPTIONS)
     stop_words = STOP_WORDS
     tag_map = TAG_MAP
     infixes = TOKENIZER_INFIXES
+    suffixes = TOKENIZER_SUFFIXES
 
 
 class Polish(Language):
